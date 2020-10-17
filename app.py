afa3044e7a2278818a4f86afe098f33edc707ed5
Daniel <wdaniel.jimenez@gmail.com>
2020-10-16 20:30:28 -0600

    Crear rutas de la api
    Se creo la ruta modificarUsuario y recuperarContra

--- app.py
+++ app.py
@@ -31,6 +31,27 @@ def crearUsuario():
     usuario.append(nuevo_usuario)
     return jsonify({'mensaje' : 'Satisfactorio, El usuario se creo correctamente'})
 
+@app.route('/modificarUsuario', methods = ['POST'])
+def modificarUsuario():
+    datos = request.get_json()
+    usuario_actual = datos['usuario_actual']
+    nombre = datos['nombre']
+    apellido = datos['apellido']
+    usuario = datos['usuario']
+    contra = datos['contra']
+    global usuarios
+    for usuario in usuarios:
+        if usuario.usuario == usuario_actual:
+            for usuario2 in usuarios:
+                if usuario2.usuario == usuario:
+                    return jsonify({'mensaje': 'Error, ya existe en la lista de usuarios el nuevo usuario, prueba otro'}), status.HTTP_400_BAD_REQUEST
+            usuario.nombre = nombre
+            usuario.apellido = apellido
+            usuario.usuario = usuario
+            usuario.contra = contra
+            return jsonify({'mensaje': 'Satisfactorio, el usuario se modificó correctamente'})
+    return jsonify({'mensaje': 'Error, el usuario no existe en la lista de usuarios'}), status.HTTP_400_BAD_REQUEST
+
 @app.route('/iniciarSesion', methods = ['GET'])
 def iniciarSesion():
     datos = request.get_json()
@@ -40,6 +61,16 @@ def iniciarSesion():
     for usuario in usuarios:
         if usuario.usuario == usuario and usuario.contra == contra:
             return jsonify({'mensaje' : 'Usuario encontrado'})
+    return jsonify({'mensaje' : 'Error, El usuario no existe'}), status.HTTP_400_BAD_REQUEST
+
+@app.route('/recuperarContra', methods = ['GET'])
+def recuperarContra():
+    datos = request.get_json()
+    usuario = datos['usuario']
+    global usuarios
+    for usuario in usuarios:
+        if usuario.usuario == usuario:
+            return jsonify({'contra' : str(usuario.conrtra)})
     return jsonify({'mensaje' : 'Error, El usuario no existe'}), status.HTTP_400_BAD_REQUEST
 
 if __name__ == "__main__":
