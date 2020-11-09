3d3884ad830ceeac5b99c30c24600b9f82d05998
Daniel <wdaniel.jimenez@gmail.com>
2020-11-08 22:26:14 -0600

    Configuración de puerto

--- app.py
+++ app.py
@@ -287,4 +287,5 @@ def cargaMasiva():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    puerto = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port = puerto)
